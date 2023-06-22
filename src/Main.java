11295c710d150b740f5cdc153d1e505ea33f351c
Desant71 <desant31081993@gmail.com>
2023-06-23 02:07:13 +0300

    long by int

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] price = {100,325,68769,3353,236878,1};
+        long[] price = {100,325,68769,3353,236878,1};
 
         SalesManager salesManager = new SalesManager(price);
 
