31b412ebab1531c0916a4cd1653684aae1858842
Desant71 <desant31081993@gmail.com>
2023-06-23 01:59:33 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] price = {100,325,68769,3353,236878,1};
+
+        SalesManager salesManager = new SalesManager(price);
+
+        System.out.println("Max значение = " + salesManager.max());
+        System.out.println("Min значение = " + salesManager.min());
+
+
+
+
+
     }
 }
